1ed4561403045473b3037893a2efff6c186e16dc
Mo Daghlas <98186599+MoDaghlas13@users.noreply.github.com>
2023-09-25 13:54:56 +0100

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -27,52 +27,51 @@ order by 2 desc, 3 desc, 4 desc
 
 
 
-  
 -- Q2 Repurchase Rate by Product
-with FirstTimePurchases as (
- select
-   Customer_ID,
-   Product_Name
- from
-   `Trinny.example`
- where
-   Order_Row_Number = 1
+WITH FirstTimePurchases AS (
+    SELECT
+        Customer_ID,
+        Product_Name
+    FROM
+        `Trinny.example`
+    WHERE
+        Product_Purchase_Number_ = 1
 ),
-  
-Repurchases as (
- select
-   f.Customer_ID,
-   f.Product_Name,
-   count(*) as repurchase_count
- from
-   FirstTimePurchases f
- left join `Trinny.example` t
- on f.Customer_ID = t.Customer_ID
-    and f.product_name = t.product_name
-    and t.order_row_number = 2
- group by f.customer_id, f.product_name
+
+Repurchases AS (
+    SELECT
+        f.Customer_ID,
+        f.Product_Name,
+        COUNT(distinct t.Customer_ID) AS repurchase_count
+    FROM
+        FirstTimePurchases f
+    LEFT JOIN `Trinny.example` t
+    ON f.Customer_ID = t.Customer_ID
+    AND f.product_name = t.product_name
+    AND t.Product_Purchase_Number_ = 2
+    GROUP BY f.customer_id, f.product_name
 ),
-  
-TotalPurchases as (
- select
-   Product_name,
-   count(*) as total_purchases
- from
-   `Trinny.example`
- group by 1
+
+TotalPurchases AS (
+    SELECT
+        Product_name,
+        COUNT(*) AS total_purchases
+    FROM
+        `Trinny.example`
+    GROUP BY 1
 )
-  
-select
- r.product_name,
- count(*) as total_first_time_purchases,
- sum (case when r.repurchase_count = 1 then 1 else 0 end) as repurchase_count,
- t.total_purchases,
- sum (case when r.repurchase_count = 1 then 1 else 0 end) / t.total_purchases as proportion_repurchased
-from
- Repurchases r
-left join TotalPurchases t
-on r.product_name = t.product_name
-group by r.product_name, t.total_purchases
+
+SELECT
+    r.product_name,
+    COUNT(*) AS total_first_time_purchases,
+    SUM(CASE WHEN r.repurchase_count = 1 THEN 1 ELSE 0 END) AS repurchase_count,
+    t.total_purchases,
+    SUM(CASE WHEN r.repurchase_count = 1 THEN 1 ELSE 0 END) / t.total_purchases AS repurchase_rate
+FROM
+    Repurchases r
+LEFT JOIN TotalPurchases t ON r.product_name = t.product_name
+GROUP BY r.product_name, t.total_purchases;
+
 
 
 
@@ -93,20 +92,20 @@ FirstTimePurchases as (
  from
    TrinnySet
  where
-   order_row_number = 1
+   variant_purchase_number = 1
 ),
   
 Repurchases as (
  select
    f.customer_id,
    f.product_variant,
-   count(*) as repurchase_count
+   count(distinct t.customer_id) as repurchase_count
  from
    FirstTimePurchases f
  left join TrinnySet t
  on f.customer_id = t.customer_id
     and f.product_variant = t.product_variant
-    and t.order_row_number = 2
+    and t.variant_purchase_number = 2
  group by f.customer_id, f.product_variant
 ),
   
@@ -124,7 +123,7 @@ select
  count(*) as total_first_time_purchases,
  sum (case when r.repurchase_count = 1 then 1 else 0 end) as repurchase_count,
  t.total_purchases,
- sum (case when r.repurchase_count = 1 then 1 else 0 end) / t.total_purchases as proportion_repurchased
+ sum (case when r.repurchase_count = 1 then 1 else 0 end) / t.total_purchases as repurchase_rate
 from
  Repurchases r
 left join TotalPurchases t
